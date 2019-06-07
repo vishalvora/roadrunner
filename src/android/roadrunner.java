79ef9a0570feb994af7ee1616b0f3859b53f4478
vishalvora <vishalgvora@gmail.com>
2019-06-07 23:03:32 +0530

    Update roadrunner.java

--- src/android/roadrunner.java
+++ src/android/roadrunner.java
@@ -29,9 +29,9 @@ public class roadrunner extends CordovaPlugin {
 
             try{
 
-                int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
-                int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
-
+              int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
+                     int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
+                
                 callback.success(""+(p1+p2));
 
             }catch(Exception ex)
